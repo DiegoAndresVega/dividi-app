7d429eb88ff5768c1ebd9c2ebbaca882f6ddbcd1
DiegoAndresVega <diegoa.vegsil@gmail.com>
2026-07-18 10:05:02 +0200

    actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// Dividi — app de gastos compartidos.
+// Autor: Diego Andres Vega Silva.
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
